68dc5196772a4f34430176fcb8c613fa75cff78a
MariaPPresa <mariaperezpresa@gmail.com>
2020-02-12 14:47:17 +0100

    Actualización librería

--- PL1/EchoVocabulary/BinaryEchoMessageCodec.cs
+++ PL1/EchoVocabulary/BinaryEchoMessageCodec.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace EchoVocabulary
 {
     public class BinaryEchoMessageCodec
     {
+
+        public byte[] Encode(EchoMessage message)
+        {
+            byte[] byteBuffer;
+
+            string _date = message.Date;
+            string _message = message.Message;
+
+            MemoryStream ms = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(ms);
+
+            writer.Write(_date);
+            writer.Write(_message);
+
+            writer.Flush();
+            byteBuffer = ms.ToArray();
+
+            return byteBuffer;
+        }
+
+        public EchoMessage Decode(Stream source)   //¿Deberiamos usar try/catch para lanzar una excepcion por si no codifica bien?
+        {
+            BinaryReader reader = new BinaryReader(source);
+
+            string read_date = reader.ReadString();
+            string read_message = reader.ReadString();
+
+            EchoMessage decoded_message = new EchoMessage(read_date, read_message);
+            return decoded_message;
+        }
     }
 }
--- PL1/EchoVocabulary/EchoMessage.cs
+++ PL1/EchoVocabulary/EchoMessage.cs
@@ -4,5 +4,27 @@ namespace EchoVocabulary
 {
     public class EchoMessage
     {
+        private string _date;
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        public string Date
+        {
+            get { return _date; }
+            set { _date = DateTime.Now.ToString("MM/dd/yyyy h:mm:ss.fff"); }
+        }
+
+        public EchoMessage(string new_date, string new_message)
+        {
+            _date = new_date;
+            _message = new_message;
+        }
+
+
     }
 }
