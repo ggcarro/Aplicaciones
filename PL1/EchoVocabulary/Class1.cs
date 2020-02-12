6a5577be51e95261c2bca988d8f1f505c341d6e6
MariaPPresa <mariaperezpresa@gmail.com>
2020-02-12 13:55:04 +0100

    * PL1.sln: * UnitTest1.cs: * Class1.cs: * EchoVocabulary.csproj: Maria
    * UnityTests.csproj:

---
+++ PL1/EchoVocabulary/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace EchoVocabulary
+{
+    public class EchoMessage
+    {
+        private string _date;
+
+        public string Message { get; set; }
+
+        //get { return _message; }
+        //set { _message = value;
+
+        public string Date
+        {
+            get { return _date; }
+            set { _date = DateTime.Now.ToString("MM/dd/yyyy h:mm:ss.fff"); }
+        }
+
+        public EchoMessage(string new_date, string new_message)
+        {
+            Date = new_date;
+            Message = new_message;
+        }
+
+    }
+
+    public interface IEchoMessageCodec
+    {
+        byte[] Encode(EchoMessage message);
+        EchoMessage Decode(Stream source);
+    }
+
+    public class BinaryEchoMessageCodec : IEchoMessageCodec
+    {
+
+        public byte[] Encode(EchoMessage message)
+        {
+            byte[] byteBuffer;
+
+            string _date = message.Date;
+            string _message = message.Message;
+
+            MemoryStream ms = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(ms);
+
+            writer.Write(_date);
+            writer.Write(_message);
+
+            writer.Flush();
+            byteBuffer = ms.ToArray();
+
+            return byteBuffer;
+        }
+
+        public EchoMessage Decode(Stream source)   //¿Deberiamos usar try/catch para lanzar una excepcion por si no codifica bien?
+        {
+            BinaryReader reader = new BinaryReader(source);
+
+            string read_date = reader.ReadString();
+            string read_message = reader.ReadString();
+
+            EchoMessage decoded_message = new EchoMessage(read_date, read_message);
+            return decoded_message;
+        }
+    }
+}
---
+++ PL1/UnityTests/UnitTest1.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+using EchoVocabulary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnityTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()   // Prueba con contenido normal 1
+        {
+            EchoMessage data = new EchoMessage("06/16/1998 9:15:45.345", "Test1 works!");
+            BinaryEchoMessageCodec codification = new BinaryEchoMessageCodec();
+
+            byte[] test1 = codification.Encode(data);
+            MemoryStream ms = new MemoryStream(test1);
+
+            EchoMessage decoded = codification.Decode(ms);
+
+            Assert.IsTrue(data.Message.CompareTo(decoded.Message) == 0);
+        }
+
+        [TestMethod]
+        public void TestMethod2()   // Prueba con contenido normal 2
+        {
+            EchoMessage data = new EchoMessage("06/05/1998 4:38:14.624", "Test2 also works!");
+            BinaryEchoMessageCodec codification = new BinaryEchoMessageCodec();
+
+            byte[] test1 = codification.Encode(data);
+            MemoryStream ms = new MemoryStream(test1);
+
+            EchoMessage decoded = codification.Decode(ms);
+
+            Assert.IsTrue(data.Message.CompareTo(decoded.Message) == 0);
+        }
+
+        [TestMethod]
+        public void TestMethod3()   // Prueba con mensaje de datos vacio
+        {
+            EchoMessage data = new EchoMessage("09/28/1996 4:38:14.624", "");
+            BinaryEchoMessageCodec codification = new BinaryEchoMessageCodec();
+
+            byte[] test1 = codification.Encode(data);
+            MemoryStream ms = new MemoryStream(test1);
+
+            EchoMessage decoded = codification.Decode(ms);
+
+            Assert.IsTrue(data.Message.CompareTo(decoded.Message) == 0);
+        }
+
+        [TestMethod]
+        public void TestMethod4()   // Prueba con formato de fecha distinto
+        {
+            EchoMessage data = new EchoMessage("3/19/1965", "Date shortcut?");
+            BinaryEchoMessageCodec codification = new BinaryEchoMessageCodec();
+
+            byte[] test1 = codification.Encode(data);
+            MemoryStream ms = new MemoryStream(test1);
+
+            EchoMessage decoded = codification.Decode(ms);
+
+            Assert.IsTrue(data.Date.CompareTo(decoded.Date) == 0);
+        }
+
+        [TestMethod]
+        public void TestMethod5()   // Prueba con mensaje de caracteres especiales
+        {
+            EchoMessage data = new EchoMessage("09/28/1996 4:38:14.624", "ñÓá#@€÷¬");
+            BinaryEchoMessageCodec codification = new BinaryEchoMessageCodec();
+
+            byte[] test1 = codification.Encode(data);
+            MemoryStream ms = new MemoryStream(test1);
+
+            EchoMessage decoded = codification.Decode(ms);
+
+            Assert.IsTrue(data.Message.CompareTo(decoded.Message) == 0);
+        }
+    }
+}
