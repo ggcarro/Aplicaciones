b53fde39d8f64c1e3db3909bf464a64f2a8d2cbb
GONZALO GARCIA CARRO <gonzalo@ggcarro.com>
2020-02-12 13:52:00 +0100

    Server actualizado

---
+++ PL1/Servidor/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Servidor
+{
+
+
+
+    class Server
+    {
+        static void Main (string[] args)
+        {
+            TcpListener listener = null;
+
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, 23456);
+                listener.Start();
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("Exception: {0}", se.Message);
+                return;
+            }
+
+            // El servidor se ejecuta infinitamente
+            for (; ; )
+            {
+                TcpClient client = null;
+                NetworkStream netStream = null;
+
+                try
+                {
+                    client = listener.AcceptTcpClient();
+                    netStream = client.GetStream();
+
+                    // Usar netStream para intercambiar información
+
+                    netStream.Close();
+                    client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: {0}", e.Message);
+                    netStream.Close();
+                }
+            }
+        }
+    }
+}
