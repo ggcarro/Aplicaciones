81037d42677a7dcc7926fbf75263235674577dff
MariaPPresa <mariaperezpresa@gmail.com>
2020-03-01 20:41:24 +0100

    Servidor ampliado

--- PL2/Server/Server.cs
+++ PL2/Server/Server.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using SNFVocabulary;
+using System.IO;
 
 namespace ServerUDP
 {
@@ -14,12 +15,13 @@ namespace ServerUDP
         private int ack;
         private int seq;
         private UdpClient udpClient;
+        StreamWriter sw = new StreamWriter("C:\\Test.txt");
 
 
         static void Main(string[] args)
         {
             Server server = new Server();
-            
+
             server.ack = 0;
             server.seq = 1;
             server.sendMessage = new SNFMessage(0, 0);
@@ -39,7 +41,7 @@ namespace ServerUDP
             // Se modificará tras la recepción (info Paquete)
             server.remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
             Console.WriteLine("Server Started");
-            // El servidor se ejecuta infinitamente
+            // El servidor se ejecuta
 
             for (; ; )
             {
@@ -53,12 +55,11 @@ namespace ServerUDP
                     //Comprobacion de que es el mensaje esperado y envio
                     server.check();
 
-                    
-                 }
-                 catch (SocketException se)
-                 {
-                     Console.WriteLine(se.ErrorCode + ": " + se.Message);
-                     return;
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                    return;
                 }
             }
         }
@@ -79,6 +80,7 @@ namespace ServerUDP
                 send();
                 Console.WriteLine("SS {0} AS {1} SR {2} AR {3}", sendMessage.Seq, sendMessage.Ack, receiveMessage.Seq, receiveMessage.Ack);
                 seq++;
+                write(receiveMessage.Data);
             }
             else
             {
@@ -87,6 +89,14 @@ namespace ServerUDP
                 Console.WriteLine("SS {0} AS {1} SR {2} AR {3}", sendMessage.Seq, sendMessage.Ack, receiveMessage.Seq, receiveMessage.Ack);
 
             }
+
+            if (receiveMessage.Seq == -1 && receiveMessage.Ack == -2)
+            {
+                Console.WriteLine("The communication is over. Client off.");
+                ack++;
+                send();
+                close();
+            }
         }
 
         private void send()
@@ -94,6 +104,15 @@ namespace ServerUDP
             byte[] sendBuffer = codec.Encode(sendMessage);
             udpClient.Send(sendBuffer, sendBuffer.Length, remoteIPEndPoint);
 
+        }
+
+        private void write(byte[] data){
+            sw.WriteLine(data);
+        }
+
+        private void close()
+        {
+            sw.Close();
         }
     }
 }
