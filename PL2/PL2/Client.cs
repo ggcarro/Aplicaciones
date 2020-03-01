620fe2592741c9eb739f617c5bdb541fd8804215
ggcarro <gonzalo@ggcarro.com>
2020-03-01 13:34:03 +0100

    Corrección del TimeOut

--- PL2/PL2/Client.cs
+++ PL2/PL2/Client.cs
@@ -8,8 +8,8 @@ namespace PL2
     class Client
     {
         private int N = 100;
-        private string ipServer = "127.0.0.1";
-        private int portServer = 23456;
+        private readonly string ipServer = "127.0.0.1";
+        private readonly int portServer = 23456;
 
         private byte[] sendPacket;
         private SNFMessage sendMessage;
@@ -19,9 +19,15 @@ namespace PL2
 
         public static void Main(string[] args)
         {
+            // Creamos objeto clase cliente;
             Client client = new Client();
             
+            // Creamos socket UDP
             UdpClient udpClient = new UdpClient();
+
+            // Asignamos TimeOut
+            udpClient.Client.ReceiveTimeout = 5000;
+
             Console.WriteLine("Client Started");
             
 
@@ -29,40 +35,49 @@ namespace PL2
             try
             {
                 client.send(1, 0, udpClient);
-                Console.WriteLine("Send: SS {0}, AS{1}", client.sendMessage.Seq, client.sendMessage.Ack);
+                Console.WriteLine("Send: Seq {0}, Ack {1}", client.sendMessage.Seq, client.sendMessage.Ack);
 
             }
             catch (Exception e)
             {
+                Console.WriteLine("Something happened when sending the first message.");
                 Console.WriteLine("Exception: {0}", e.Message); // Otro tipo de error
+                Environment.Exit(0); // Finalizamos el programa
             }
+
             int i = 1;
             int nTimeOut = 0;
+            possibleStates state;
             bool timeOut;
 
             while(i<client.N && nTimeOut<10)
             {
                 
                 //Recibo
-                timeOut=client.receive(udpClient);
+                state=client.receive(udpClient);
 
                 // Si hay timeOut vuelvo a enviar
-                if (timeOut == false)
+                if (state == possibleStates.TimeOut)
                 {
                     nTimeOut++;
                     client.send(i, i-1, udpClient);
                     Console.WriteLine("TimeOut {0} SS {1}, SA {2}", nTimeOut, client.sendMessage.Seq, client.sendMessage.Ack);
                 }
-                if(timeOut== true)
+
+                if(state == possibleStates.ReceiveOk)
                 {
+                    // Reseteamos TimeOut
                     nTimeOut = 0;
-                    // compruebo
+
+                    // Comprobamos que es el paquete deseado
                     if (client.checkMessage())
                     {
-                        Console.WriteLine("OK SS {0} AS {1} SR {2} AR {3}", client.sendMessage.Seq, client.sendMessage.Ack, client.receiveMessage.Seq, client.receiveMessage.Ack);
+                        // Aumentamos numero de seq
                         i++;
+
+                        // Generamos numero aleatorio para simular posible pérdida
                         Random rd = new Random();
-                        if (rd.Next(0, 10) > 5)
+                        if (rd.Next(0, 10) > 2)
                         {
                             client.send(i, i - 1, udpClient);
                             Console.WriteLine("Send: SS {0}, AS {1}", client.sendMessage.Seq, client.sendMessage.Ack);
@@ -72,15 +87,34 @@ namespace PL2
                             Console.WriteLine("Lost Packet SS: {0}, AS {1}", client.sendMessage.Seq, client.sendMessage.Ack);
                         }
                     }
+
                     else
                     {
                         Console.WriteLine("Packet not Expected:  SS {0} AS {1} SR {2} AR {3}", client.sendMessage.Seq, client.sendMessage.Ack, client.receiveMessage.Seq, client.receiveMessage.Ack);
-
+                        // ¿enviamos el que el cliente desea?
                     }
                 }
             }
 
+
+            // Enviamos fin y no hace falta esperar a confirmación.
+            client.send(-1, 0, udpClient);
             
+
+            if(nTimeOut >= 10)
+            {
+                Console.WriteLine("Maximum number of timeout attempts");
+                Console.WriteLine("Check the conexion with the server");
+            }
+
+            else
+            {
+                Console.WriteLine("It seems that everything worked correctly");
+            }
+
+            udpClient.Close();
+            Environment.Exit(0);
+
         }
 
         private void send(int seq, int ack, UdpClient udpClient)
@@ -90,34 +124,35 @@ namespace PL2
             udpClient.Send(sendPacket, sendPacket.Length, ipServer, portServer);
         }
 
-        private bool receive(UdpClient udpClient)
+        private possibleStates receive(UdpClient udpClient)
         {
-            var timeToWait = TimeSpan.FromSeconds(5);
-            var asyncResult = udpClient.BeginReceive(null, null);
-            asyncResult.AsyncWaitHandle.WaitOne(timeToWait);
-            if (asyncResult.IsCompleted)
+            try
             {
-                try
-                {
-                    IPEndPoint remoteEP = null;
-                    byte[] receivedData = udpClient.EndReceive(asyncResult, ref remoteEP);
-                    receiveMessage = codec.Decode(receivedData);
-                    return true;
-                    
-                }
-                catch (Exception ex)
-                {
-                    // EndReceive failed and we ended up here
-                    Console.WriteLine(ex.Message);
-                    return false;
-                }
+                // Recibir datos
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] receivedData = udpClient.Receive(ref remoteEP);
+                receiveMessage = codec.Decode(receivedData);
+                return possibleStates.ReceiveOk;
             }
-            else
+            catch (SocketException e)
             {
-                // The operation wasn't completed before the timeout and we're off the hook
-                // Console.WriteLine("Timeout");
-                return false;
+                if (e.SocketErrorCode == SocketError.TimedOut)
+                {
+                    // Timeout
+                    Console.WriteLine("TimeOut Error");
+                    return possibleStates.TimeOut;
+                }
+
+                else
+                {
+                    // Otro error
+                    Console.WriteLine("Server seems to be disconnected");
+                    return possibleStates.Error;
+                }
+
             }
+        
+            
         }
 
         
@@ -132,7 +167,13 @@ namespace PL2
                 return false;
             }
         }
-        
+
+        private enum possibleStates
+        {
+            ReceiveOk,
+            TimeOut,
+            Error,
+        }
 
 
     }
