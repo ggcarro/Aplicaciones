269e28defa381fc5517e8e51703391d6809d155c
ggcarro <gonzalo@ggcarro.com>
2020-03-31 22:30:17 +0200

    AmigosBase Completado

--- Amigos/Amigos/Models/Amigo.cs
+++ Amigos/Amigos/Models/Amigo.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Amigos.Models
 {
     public class Amigo
     {
         public int ID { get; set; }
+        [Display(Name = "Nombre")]
         public string name { get; set; }
+        [Display(Name = "Longitud")]
         public string longi { get; set; }
+        [Display(Name = "Latitud")]
         public string lati { get; set; }
     }
 }
