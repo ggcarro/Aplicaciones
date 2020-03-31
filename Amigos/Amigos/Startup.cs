3bfcff4462c544e62448f049c479e5d9717c5a13
ggcarro <gonzalo@ggcarro.com>
2020-03-31 21:29:31 +0200

    Se realizan las tareas 2.8

--- Amigos/Amigos/Startup.cs
+++ Amigos/Amigos/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amigos.DataAccessLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -32,6 +34,8 @@ namespace Amigos
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<AmigoDBContext>(options => options.UseSqlite("Data Source=Amigos.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
