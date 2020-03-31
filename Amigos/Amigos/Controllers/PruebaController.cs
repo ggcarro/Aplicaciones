eb348daa4c8739c68b20888e6fe7a230524141c0
MariaPPresa <mariaperezpresa@gmail.com>
2020-03-31 18:59:05 +0200

    Me obligan a decir algo... hola

---
+++ Amigos/Amigos/Controllers/PruebaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amigos.Controllers
+{
+    public class PruebaController : Controller
+    {
+        private readonly IInc _inc;
+
+        public PruebaController(IInc inc)
+        {
+            _inc = inc;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string hola(string valor, int veces)
+        {
+            string repeated = "";
+            for (int i = 0; i < veces; i++)
+            {
+                repeated += valor;
+            }
+            return repeated;
+        }
+
+    }
+}
---
+++ Amigos/Amigos/IncImp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amigos
+{
+    public class IncImpl : IInc
+    {
+        int x = 0;
+
+        public IncImpl()
+        {
+            x = 0;
+        }
+        public int Inc()
+        {
+            x++;
+            return x;
+        }
+    }
+
+}
