a0cd1bb9fa49279e9e0e0177a5678b42e5f748bf
ggcarro <gonzalo@ggcarro.com>
2020-04-08 13:40:33 +0200

    PL 3.1  -- Se realiza todos los apartados hasta el 2.4 (incluido) [Para ejecutar lanzar el servicio con visual studio y luego ir a la carpeta de MathClientWpf/Bin/Debug y ejecutar el programa de escritorio

---
+++ MathService/MathClientWpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathClientWpf
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int iValue;
+            Int32.TryParse(Valor.Text, out iValue);
+            MathService.MathClient client = new MathService.MathClient();
+
+            bool result = client.Prime(iValue);
+
+            if (result == true)
+            {
+                Resultado.Content = "Es primo";
+            }
+            else
+            {
+                Resultado.Content = "No es primo";
+            }
+        }
+    }
+}
---
+++ MathService/MathService/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathService
+{
+    class Math : IMath
+    {
+        public bool Prime(int value)
+        {
+            int divider = 2;
+            int remainder = 0;
+            while (divider < value)
+            {
+                remainder = value % divider;
+                if (remainder == 0)
+                {
+                    return false;
+                }
+                divider++;
+            }
+            return true;
+        }
+    }
+}
