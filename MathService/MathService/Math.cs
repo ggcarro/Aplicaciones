f44cf287925682463ddc40609f36dda46954837a
MariaPPresa <mariaperezpresa@gmail.com>
2020-04-10 18:18:30 +0200

    Da problemas el (using System.ServiceModel) Si no, el 2.6 estaría

--- MathService/MathService/Math.cs
+++ MathService/MathService/Math.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MathService
 {
-    class Math : IMath
+    public class Math : IMath
     {
         public bool Prime(int value)
         {
---
+++ MathService/MathServiceHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace MathServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MathService.Math)))
+            {
+                host.Open();
+
+                Console.WriteLine("Servicio ejecutándose. Pulsa <enter- para terminar");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
