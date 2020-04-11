8d607c899ad63b5f7a236ad48158aa8f5bbc71d7
ggcarro <gonzalo@ggcarro.com>
2020-04-11 12:40:09 +0200

    Apartado 2.7 completado a falta de añadir el netTCPenpoint que da fallos -- He escrito ahora a Carlos para ver si él sabe lo que estoy haciendo mal

---
+++ MathServiceHost/MathClientConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClient client = new MathClient("netTcpBinding_IMath");
+
+            int x = 23;
+
+            if (client.Prime(x))
+                Console.WriteLine("{0} es primo", x);
+            else
+                Console.WriteLine("{0} no es primo", x);
+        }
+    }
+}
