b7f5098b75b9f74bc6c8b67484074ce8be3d3523
ggcarro <gonzalo@ggcarro.com>
2020-04-20 18:19:33 +0200

    Revert "Apartado 2.3"
    This reverts commit 693b48e0b98d066e2dfb15409d686455f1de7afa.

--- WebApi/WebApi/Startup.cs
+++ WebApi/WebApi/Startup.cs
@@ -25,7 +25,6 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddMvc().AddXmlSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
