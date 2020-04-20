4f8ac147c27d1871e6359fdde563d93d14ef75b8
ggcarro <gonzalo@ggcarro.com>
2020-04-20 13:14:07 +0200

    Revert "Revert "Revert "."""
    This reverts commit e84c2e5cbbddfe1cedff279a85d06a5ec399d742.

---
+++ Amigos/Amigos/Migrations/20200331192745_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Amigos.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amigos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(nullable: true),
+                    longi = table.Column<string>(nullable: true),
+                    lati = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amigos", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Amigos");
+        }
+    }
+}
