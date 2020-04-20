90e0497bdad6dca5ecdfd1a2d4092b3bf898c357
ggcarro <gonzalo@ggcarro.com>
2020-04-20 13:11:14 +0200

    Revert "."
    This reverts commit c4fe7a4ec24768bccfe7c8c5a7f7413c7a0106ac.

---
+++ Amigos/Amigos/Controllers/PruebaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Amigos.Controllers
+{
+    public class PruebaController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hola(string valor, int veces)
+        {
+            string str = "";
+            for(int i=0; i<veces; i++)
+            {
+                str = str + " " + valor;
+            }
+            return str;
+        }
+        public IActionResult Adios(string valor, int veces)
+        {
+            ViewBag.valor = valor;
+            ViewBag.valor = veces;
+
+            return View();
+
+        }
+    }
+}
