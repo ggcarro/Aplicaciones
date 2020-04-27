0b8b9388e3f7def757dbcb9866d23545ce95c8f1
ggcarro <gonzalo@ggcarro.com>
2020-04-27 21:04:15 +0200

    2.3 Terminado

---
+++ Progeto/Progeto.API/Controllers/ProgetoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Progeto.Lua;
+using Progeto.API.Models;
+
+namespace Progeto.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgetoController : ControllerBase
+    {
+        LuaInterpreter interpreter = new LuaInterpreter();
+
+        [HttpPost]
+        public Answer Post([FromBody] ProgetoModel program)
+        {
+            // Iniciamos temporizador 
+            Stopwatch temp = new Stopwatch();
+            temp.Start();
+            string[] intResult = interpreter.RunProgram(program.code).ToArray();
+            temp.Stop();
+            return new Answer(intResult[0], intResult[1], temp.Elapsed.TotalMilliseconds.ToString());
+        }
+    }
+}
---
+++ Progeto/Progeto.API/Models/ProgetoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Progeto.API.Models
+{
+    public class ProgetoModel
+    {
+        public string code { get; set; }
+    }
+}
