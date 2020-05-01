a760c313c5bfcb651a881454677120990736f438
ggcarro <gonzalo@ggcarro.com>
2020-05-01 12:30:56 +0200

    RabbitMQ

---
+++ RabbitMQ/Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definimos IP y creamos conexión
+            var factory = new ConnectionFactory() { HostName = "10.115.1.81" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("ColaAT", false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine("Recibido {0}", message);
+                    };
+                    channel.BasicConsume(queue: "ColaAT",
+                                      autoAck: true,
+                                      consumer: consumer);
+                    Console.WriteLine("Press enter to exit");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ/Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definimos IP y creamos conexión
+            var factory = new ConnectionFactory() { HostName = "10.115.1.81" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("ColaAT", false, false, false, null);
+
+                    string message = DateTime.Now.ToString() + " - Mensaje de prueba";
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "ColaAT", null, body);
+
+                    Console.WriteLine("Enviado el mensaje: {0}", message);
+                }
+            }
+        }
+    }
+}
